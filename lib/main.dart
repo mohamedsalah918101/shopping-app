211e472629b2be0dcf971153ea77f649be85f72c
mohamed_salah <mohamedsalah3072001@gmail.com>
2025-01-29 20:05:15 +0200

    Adding Firebase Auth to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/login_page.dart';
-import 'package:shopping_app/signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
